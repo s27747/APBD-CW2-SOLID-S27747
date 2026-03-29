868e0a4d3fc25146666797a7c6c9d602e1d5a6d6
Jakub Erbetowski <s27747@pjwstk.edu.pl>
2026-03-29 21:32:40 +0200

    Add console demo

--- APBD-CW2-SOLID-S27747/Services/RentalService.cs
+++ APBD-CW2-SOLID-S27747/Services/RentalService.cs
@@ -12,17 +12,20 @@ public class RentalService
     private readonly IUserRepository _userRepository;
     private readonly IEquipmentRepository _equipmentRepository;
     private readonly IUserLoanLimitPolicy _userLoanLimitPolicy;
+    private readonly ILateReturnPenaltyPolicy _lateReturnPenaltyPolicy;
 
     public RentalService(
         ILoanRepository loanRepository,
         IUserRepository userRepository,
         IEquipmentRepository equipmentRepository,
-        IUserLoanLimitPolicy userLoanLimitPolicy)
+        IUserLoanLimitPolicy userLoanLimitPolicy,
+        ILateReturnPenaltyPolicy lateReturnPenaltyPolicy)
     {
         _loanRepository = loanRepository;
         _userRepository = userRepository;
         _equipmentRepository = equipmentRepository;
         _userLoanLimitPolicy = userLoanLimitPolicy;
+        _lateReturnPenaltyPolicy = lateReturnPenaltyPolicy;
     }
 
     public OperationResult<Loan> BorrowEquipment(int userId, int equipmentId, DateTime borrowedAt, int days)
@@ -60,4 +63,38 @@ public class RentalService
     {
         return _loanRepository.GetActiveByUserId(userId);
     }
+    
+    public OperationResult<Loan> ReturnEquipment(int loanId, DateTime returnDate)
+    {
+        var loan = _loanRepository.GetById(loanId);
+
+        if (loan is null)
+            return OperationResult<Loan>.Fail("Loan not found.");
+
+        if (!loan.IsActive)
+            return OperationResult<Loan>.Fail("This loan is already closed.");
+
+        if (returnDate.Date < loan.BorrowedAt.Date)
+            return OperationResult<Loan>.Fail("Return date cannot be earlier than borrow date.");
+
+        var penalty = _lateReturnPenaltyPolicy.CalculatePenalty(loan.DueDate, returnDate);
+
+        loan.CompleteReturn(returnDate, penalty);
+        loan.Equipment.MarkAsAvailable();
+
+        return OperationResult<Loan>.Ok(
+            $"Equipment {loan.Equipment.Name} was returned. Penalty: {penalty:C}.",
+            loan);
+    }
+
+    public IReadOnlyList<Loan> GetOverdueLoans(DateTime currentDate)
+    {
+        return _loanRepository
+            .GetActive()
+            .Where(l => l.IsOverdue(currentDate))
+            .OrderBy(l => l.DueDate)
+            .ToList()
+            .AsReadOnly();
+    }
+    
 }
